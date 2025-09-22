e9f7c505f7bb8629a404bcea690dfbee67c9aae4
Anish <anishkumar.s2005@gmail.com>
2025-09-22 19:20:46 +0530

    updated code

--- zeninfo.py
+++ zeninfo.py
@@ -133,7 +133,7 @@ class MainWindow(qtw.QMainWindow):
                 container.append(str(int(element.checkbox.isChecked())))
                 element = element.left
             data.append(''.join(container))
-            with open('/home/anish/project/dypatil-food-project/zendata.csv', 'a') as file:
+            with open('zendata.csv', 'a') as file:
                 csvwriter = csv.writer(file)
                 csvwriter.writerow(data)
             clear()
